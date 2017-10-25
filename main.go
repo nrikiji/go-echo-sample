df85be7f1bf38b0cee91731ed1904b7efc645af6
nrikiji <nrikiji@gmail.com>
2017-10-25 11:42:32 +0900

    start_server導入 graceful対応

--- main.go
+++ main.go
@@ -3,28 +3,52 @@ package main
 import (
 	"app/route"
 	"app/handler"
-	"app/log"
+	// "app/log"
 	"app/model"
 	"app/config"
+	"context"
+	// "net/http"
+	"os"
+	"os/signal"
+	"time"
+	"fmt"
 	"flag"
+	"github.com/lestrrat/go-server-starter/listener"
 )
 
 func main() {
 
 	setConfig()
 
-	router := route.Init()
+	e := route.Init()
 
 	// router.Use(middleware.Logger())
 	// router.Use(middleware.Recover())
 
+  listeners, err := listener.ListenAll()
+  if err != nil {
+      panic(err)
+  }
+	e.Listener = listeners[0]
+
 	model.Init()
+	e.HTTPErrorHandler = handler.JSONErrorHandler
 
-	router.HTTPErrorHandler = handler.JSONErrorHandler
+	go func() {
+		// if err := e.Start(":8080"); err != nil {
+		if err := e.Start(""); err != nil {
+			panic(fmt.Sprintf("[Error]: %s", err))
+		}
+	}()
 
-	log.AppLog.Info("サーバーが起動しました")
-
-	router.Start(":8080")
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 	
 }
 
